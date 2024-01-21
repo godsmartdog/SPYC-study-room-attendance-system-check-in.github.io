c9340f46a6ca5ad8f658cf4a080c107d6f1d4b2c
godsmartdog <132557080+godsmartdog@users.noreply.github.com>
2024-01-21 21:00:16 +0800

    Update and rename checkIn.deno to checkIn.js

--- checkIn.js
+++ checkIn.js
@@ -1,5 +1,5 @@
 import 'https://unpkg.com/@supabase/supabase-js@2'
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+Deno.import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 
 
