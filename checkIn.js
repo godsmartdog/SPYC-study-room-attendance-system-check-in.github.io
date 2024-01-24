64d4aaab65bc21598e3dfa078083cc8a1eaceeed
godsmartdog <godsmartdog@gmail.com>
2024-01-24 16:52:08 +0800

    Your commit message

--- checkIn.js
+++ checkIn.js
@@ -1,18 +1,16 @@
-//import { createClient } from '@supabase/supabase-js';
-//const { createClient} = supabase
-//const supabaseUrl = 'https://puisbpdboykphyeexnrh.supabase.co'
-//const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1aXNicGRib3lrcGh5ZWV4bnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NTUwMDEsImV4cCI6MjAyMTIzMTAwMX0.Sl_aehSlK5xgim5BoGfD4IAezVMuKEi77XmUW2_yRWw'
-
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-//const supabase = createClient('https://puisbpdboykphyeexnrh.supabase.co', 
-//'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1aXNicGRib3lrcGh5ZWV4bnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NTUwMDEsImV4cCI6MjAyMTIzMTAwMX0.Sl_aehSlK5xgim5BoGfD4IAezVMuKEi77XmUW2_yRWw')
 
-
-
-
+//do not write anything outside async
 
 (async () => {
   //console.log('Supabase Instance: ', supabase)
+
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: 'godsmartdog@email.com',
+    password: 'pyc18076',
+  })
+
   const supabase = createClient('https://puisbpdboykphyeexnrh.supabase.co', 
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1aXNicGRib3lrcGh5ZWV4bnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NTUwMDEsImV4cCI6MjAyMTIzMTAwMX0.Sl_aehSlK5xgim5BoGfD4IAezVMuKEi77XmUW2_yRWw')
   checkInForm.addEventListener('submit', async (event) => {
@@ -31,15 +29,10 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
         }
       ]);
 
-    //if (error) {
-    //  console.error(error);
-    //  return;
-    //}
-
     //clear input field
     attendeeIdInput.value = '';
 
     //useless, for me to check
-    //console.log('Check-in successful!');
+    console.log('Check-in successful!');
   });
 })();
