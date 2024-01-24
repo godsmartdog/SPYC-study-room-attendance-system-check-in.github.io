1c867f2f4d4bf338aa32a1de3dfb829f3fa39073
godsmartdog <godsmartdog@gmail.com>
2024-01-24 16:54:05 +0800

    Your commit message

--- checkIn.js
+++ checkIn.js
@@ -6,13 +6,14 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
   //console.log('Supabase Instance: ', supabase)
 
 
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email: 'godsmartdog@email.com',
-    password: 'pyc18076',
-  })
+  
 
   const supabase = createClient('https://puisbpdboykphyeexnrh.supabase.co', 
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1aXNicGRib3lrcGh5ZWV4bnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NTUwMDEsImV4cCI6MjAyMTIzMTAwMX0.Sl_aehSlK5xgim5BoGfD4IAezVMuKEi77XmUW2_yRWw')
+  const { data, error } = await supabase.auth.signInWithPassword({
+  email: 'godsmartdog@email.com',
+  password: 'pyc18076',
+})  
   checkInForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     const attendeeIdInput = document.getElementById('attendeeId');
