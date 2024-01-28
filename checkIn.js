3859b5866cfa6dd15408bd9baaf71fdb89376258
godsmartdog <132557080+godsmartdog@users.noreply.github.com>
2024-01-28 15:43:16 +0800

    Update checkIn.js

--- checkIn.js
+++ checkIn.js
@@ -5,25 +5,21 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 (async () => {
 
   const supabase = createClient('https://puisbpdboykphyeexnrh.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1aXNicGRib3lrcGh5ZWV4bnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NTUwMDEsImV4cCI6MjAyMTIzMTAwMX0.Sl_aehSlK5xgim5BoGfD4IAezVMuKEi77XmUW2_yRWw')
-  //const { data, error } = await supabase.auth.signInWithPassword({
-  //  email: 'godsmartdog@email.com',
-  //  password: 'pyc18076',
-  //  })  
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1aXNicGRib3lrcGh5ZWV4bnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NTUwMDEsImV4cCI6MjAyMTIzMTAwMX0.Sl_aehSlK5xgim5BoGfD4IAezVMuKEi77XmUW2_yRWw') 
   const checkInForm = document.getElementById('checkInForm');
-  const attendeeId = document.getElementById('attendeeId');
+  const attendeeId = document.getElementById('attendeeId') //the input (add .value if you need to use the value inside the input)
   const attendanceTime = new Date().toISOString(); // Get current time in ISO 8601 format
 
   attendeeId.addEventListener('keydown', function (e) {
     if (e.key == "Enter") {
       console.log("HIHIHI")
-      // checkInForm.submit();
+      // only for check, not important
     }
 
   });
   const handleFormSubmit = async (event) => {
 
-    // Insert record
+    // Insert record in check_ins, for the history
     const { data, error } = await supabase
       .from('check_ins')
       .insert([
@@ -32,19 +28,19 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
           attendance_time: attendanceTime
         }
       ]);
-    //check is there the record exist
+    //check is there the record exist in room status for checking he is entering or leaving
     const { data: existingRecords, fetch_error } = await supabase
       .from('roomStatus')
       .select()
       .eq('attendee_id', attendeeId.value);
 
-    let checkInTime =null
+    let checkInTime =null //for calculate the total time
 
     if (existingRecords && existingRecords.length > 0) {
 
       checkInTime = new Date(existingRecords[0].check_in_time);
 
-      // If record exists, delete it
+      // If record exists, delete it, so it won't show in roomStatus
       const { fetch_error: deleteError } = await supabase
         .from('roomStatus')
         .delete()
@@ -69,13 +65,13 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
       console.log('Record inserted into roomStatus');;
     }
 
-    // Insert record into ranking table if it doesn't exist
+    // Insert record into ranking table if it doesn't exist, for first time login
     const { data: existingRanking } = await supabase
     .from('ranking')
     .select('*')
     .eq('attendee_Id', attendeeId.value);
 
-    if (!existingRanking || existingRanking.length === 0) {
+    if (!existingRanking || existingRanking.length === 0) { //same as roomStatus
     const { error: rankingInsertError } = await supabase
     .from('ranking')
     .insert([
@@ -89,10 +85,10 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
     let totalTime = 0;
     if (checkInTime) {
       const currentTime = new Date();
-      totalTime = (currentTime - checkInTime)/3600000;
+      totalTime = (currentTime - checkInTime)/3600000; // get hour for total time
     }
 
-    // Retrieve previous total time from ranking table
+    // Retrieve previous total time from ranking table, to add them up
     const { data: rankingData, error: rankingError } = await supabase
       .from('ranking')
       .select('total_time')
@@ -112,17 +108,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 
 
-    } else {
-      // If record doesn't exist, insert it as the first attendance
-      const { error: insertError } = await supabase
-        .from('roomStatus')
-        .insert([
-          {
-            attendee_id: attendeeId.value,
-            check_in_time: new Date().toISOString()
-          }
-        ]);
-    }
+    } 
 
 
     console.log('Check-in successful!');
