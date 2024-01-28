b2df8839d0a99daa8d495370fddf8de7a22931e5
godsmartdog <132557080+godsmartdog@users.noreply.github.com>
2024-01-28 15:56:21 +0800

    Update checkIn.js

--- checkIn.js
+++ checkIn.js
@@ -5,7 +5,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 (async () => {
 
   const supabase = createClient('https://puisbpdboykphyeexnrh.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1aXNicGRib3lrcGh5ZWV4bnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NTUwMDEsImV4cCI6MjAyMTIzMTAwMX0.Sl_aehSlK5xgim5BoGfD4IAezVMuKEi77XmUW2_yRWw') 
+    'the api key') 
   const checkInForm = document.getElementById('checkInForm');
   const attendeeId = document.getElementById('attendeeId') //the input (add .value if you need to use the value inside the input)
   const attendanceTime = new Date().toISOString(); // Get current time in ISO 8601 format
