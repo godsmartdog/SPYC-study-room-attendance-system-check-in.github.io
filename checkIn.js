b7e9c056bc303ce6872ee59cbc8e4c1ae9bfd1fb
godsmartdog <132557080+godsmartdog@users.noreply.github.com>
2024-02-02 17:19:51 +0800

    Update checkIn.js

--- checkIn.js
+++ checkIn.js
@@ -7,11 +7,11 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
   const supabase = createClient('https://puisbpdboykphyeexnrh.supabase.co',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1aXNicGRib3lrcGh5ZWV4bnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU2NTUwMDEsImV4cCI6MjAyMTIzMTAwMX0.Sl_aehSlK5xgim5BoGfD4IAezVMuKEi77XmUW2_yRWw') 
   const checkInForm = document.getElementById('checkInForm');
-  let attendee_ID = document.getElementById('attendeeId') //the input (add .value if you need to use the value inside the input)
+  let attendeeId = document.getElementById('attendeeId') //the input (add .value if you need to use the value inside the input)
   let attendanceTime = new Date().toISOString(); // Get current time in ISO 8601 format
   let input_valid = true; //for status message
-  let attendeeId =attendee_ID.value
-  attendee_ID.addEventListener('keydown', function (e) {
+  
+  attendeeId.addEventListener('keydown', function (e) {
     if (e.key == "Enter") {
       console.log("HIHIHI")
       // only for check, not important
@@ -23,17 +23,18 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
     // Insert record in check_ins, for the history
     const { data, error } = await supabase
       .from('check_ins')
-      .insert([
+      .upsert([
         {
-          Attendee_Id: attendeeId,
+          Attendee_Id: attendeeId.value,
           attendance_time: attendanceTime
         }
       ])
+      console.log('Record insert in check_ins');
     //check is there the record exist in room status for checking he is entering or leaving
     const { data: existingRecords, fetch_error } = await supabase
       .from('roomStatus')
       .select()
-      .eq('attendee_id', attendeeId)
+      .eq('attendee_id', attendeeId.value)
 
     let checkInTime =null //for calculate the total time
 
@@ -45,7 +46,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
       const { fetch_error: deleteError } = await supabase
         .from('roomStatus')
         .delete()
-        .eq('attendee_id', attendeeId)
+        .eq('attendee_id', attendeeId.value)
 
       console.log('Record deleted from roomStatus');
     }
@@ -57,7 +58,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
         .from('roomStatus')
         .insert([
           {
-            attendee_id: attendeeId,
+            attendee_id: attendeeId.value,
             status: 'checked-in',
             check_in_time : attendanceTime,
           }
@@ -70,14 +71,14 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
     const { data: existingRanking } = await supabase
     .from('ranking')
     .select('*')
-    .eq('attendee_Id', attendeeId);
+    .eq('attendee_Id', attendeeId.value);
 
     if (!existingRanking || existingRanking.length === 0) { //same as roomStatus
     const { error: rankingInsertError } = await supabase
     .from('ranking')
     .insert([
       {
-        attendee_Id: attendeeId,
+        attendee_Id: attendeeId.value,
         total_time: 0, // Set the current timestamp as total_time
       },
     ])};
@@ -93,7 +94,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
     const { data: rankingData, error: rankingError } = await supabase
       .from('ranking')
       .select('total_time')
-      .eq('attendee_Id', attendeeId)
+      .eq('attendee_Id', attendeeId.value)
 
     let previousTotalTime = 0;
 
@@ -104,13 +105,13 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
       const { error: updateError } = await supabase
         .from('ranking')
         .update({ total_time: previousTotalTime + totalTime })
-        .eq('attendee_Id', attendeeId)
+        .eq('attendee_Id', attendeeId.value)
 
     } 
 
 
     console.log('Check-in successful!');
-    attendee_ID.value = ''
+    
 
   };
   
@@ -157,32 +158,33 @@ function clearRanking(){
         const startTime = new Date();
         startTime.setHours(8, 15, 0); // Set the start time to 08:15:00
         const endTime = new Date();
-        endTime.setHours(18, 30, 0); // Set the end time to 18:30:00
+        endTime.setHours(16, 30, 0); // Set the end time to 18:30:00
         
         if (currentDate <= startTime || currentDate >= endTime) {
-      
-      
-          console.log("hi5")
           let { data: roomStatus, error } = await supabase
           .from('roomStatus')
           .select('attendee_id')
-          console.log("hi2")
-          roomStatus.forEach(async (attendee) => {
-          for (let i = 0; i < 2*data.length; i++) {
-            const tmp_time = new Date();
-            tmp_time.setHours(18, 30, 0); // Set the start time to 08:15:00
+          let num_of_loop =roomStatus.length
 
-            const timestamp = tmp_time.getTime();
+          for (let i=0;i<=num_of_loop;i++); {
+          let { data: roomStatus, error } = await supabase
+          .from('roomStatus')
+          .select('attendee_id')
+        
+         
+          
+          roomStatus.forEach(async (attendee) => {
+            const tmp_time = new Date();
             attendanceTime = new Date(timestamp).toISOString();
-           
-            
-            attendeeId = attendee.attendee_id;
-  
+            attendeeId.value = attendee.attendee_id;
+            document.getElementById("attendeeId").click();
             console.log("hi3");
             await handleFormSubmit();
             console.log("hi4");
-        }});
         }
+        )
+        } }
+        
       })();
     }
 
@@ -195,11 +197,11 @@ function clearRanking(){
     const startTime = new Date();
     startTime.setHours(8, 15, 0); // Set the start time to 08:15:00
     const endTime = new Date();
-    endTime.setHours(16, 30, 0); // Set the end time to 18:30:00
+    endTime.setHours(18, 30, 0); // Set the end time to 18:30:00
      if (currentDate >= startTime && currentDate <= endTime) {
-      let attendeeId =attendee_ID.value
+      
       await handleFormSubmit();
-      attendeeId.value = '';}
+      }
     else{
       input_valid = false;
     }
@@ -210,11 +212,10 @@ function clearRanking(){
     
       
     //clear input field
-    attendee_ID.value = '';
+    attendeeId.value = '';
 
 
   })
   setInterval(checkRewardEligibility, 1000 * 60 * 60 * 24 ); 
-  setInterval(timeLimitCheckIn, 1000*60*30); 
+  setInterval(timeLimitCheckIn, 1000*60); 
 })();
-
